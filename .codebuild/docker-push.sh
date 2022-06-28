d4c2ccef5c6e01ad344bdf76680c6dbe15a4f1d2
Aliaksei Malyshau <aliaksei.malyshau@altoros.com>
2022-06-28 14:14:41 +0200

    fix: Added slashes to push .sh script

--- .codebuild/docker-push.sh
+++ .codebuild/docker-push.sh
@@ -4,12 +4,10 @@
 repository=$1
 
 # Push Docker images to the repository
-now=`date +%Y-%m-%d--%H-%M-%S`
-for image in walletconnect_nginx walletconnect_bridge
-do
-  docker tag $image:latest $repository-$image:latest
-  docker push $repository-$image:latest
-  docker tag $image:latest $repository-$image:$now
-  docker push $repository-$image:$now
+now=$(date +%Y-%m-%d--%H-%M-%S)
+for image in walletconnect_nginx walletconnect_bridge; do
+  docker tag $image:latest $repository/$image:latest
+  docker push $repository/$image:latest
+  docker tag $image:latest $repository/$image:$now
+  docker push $repository/$image:$now
 done
-
